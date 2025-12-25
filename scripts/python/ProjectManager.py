fa8e8057c60bd4c26d55e61d3e5735d592d978de
Epslin <60341743+jiadevr@users.noreply.github.com>
2025-12-25 19:06:14 +0800

    添加AddScene

--- scripts/python/ProjectManager.py
+++ scripts/python/ProjectManager.py
@@ -6,17 +6,28 @@ import shutil
 from PySide6 import QtCore,QtUiTools,QtWidgets,QtGui
 
 class ProjectManager(QtWidgets.QMainWindow):
+    # CLASS Constant
+
     def __init__(self) -> None:
         super().__init__()
-        project_description_path=hou.text.expandString("$MYLIB")+"configs/ProjectConfig.json"
-        ui_path=hou.text.expandString("$MYLIB")+"/ui/project_manager.ui"
-        print(ui_path)
+        ui_path=hou.text.expandString("$MYLIB/ui/project_manager.ui").replace(os.sep,"/")
         self.ui=QtUiTools.QUiLoader().load(ui_path,parentWidget=self)
         self.setParent(hou.qt.mainWindow(),QtCore.Qt.Window)
         self.setWindowTitle("Project Manager")
         self.setMaximumSize(600,400)
 
-        # 获取各对象引用
+        self._InitialUI_()
+        self._SetupBindings_()
+
+        self.config_file_path=hou.text.expandString("$MYLIB/configs/ProjectConfig.json").replace(os.sep,"/")
+        
+        # 读取Json信息
+        self.LoadProjectsFromConfig()
+
+    def _InitialUI_(self):
+        '''
+        Setup UI Element Ref
+        '''
         self.lw_project:QtWidgets.QListWidget=self.ui.findChild(QtWidgets.QListWidget,"lw_projects")
 
         self.project_detail:QtWidgets.QPushButton=self.ui.findChild(QtWidgets.QPushButton,"bt_proj_details")
@@ -33,29 +44,61 @@ class ProjectManager(QtWidgets.QMainWindow):
         self.save_file:QtWidgets.QPushButton=self.ui.findChild(QtWidgets.QPushButton,"bt_save")
 
         self.state_line:QtWidgets.QLineEdit=self.ui.findChild(QtWidgets.QLineEdit,"lineEdit")
-        
-        
-        # 读取Json信息
-        self.LoadProjectsFromConfig()
 
-        # 绑定按键行为
+    def _SetupBindings_(self):
+        '''
+        SetupUI Binding To Functions
+        '''
         self.project_detail.clicked.connect(self.ShowProjectDetails)
         self.project_enable.clicked.connect(lambda:self.ToggleProjectEnable(True))
         self.project_disable.clicked.connect(lambda:self.ToggleProjectEnable(False))
         self.delete_project.clicked.connect(self.DeleteProject)
 
-        self.lw_project.itemSelectionChanged.connect(self.RefreshSceneList)
-        #self.create_scene.clicked.connect()
+        self.lw_project.currentItemChanged.connect(self.RefreshSceneList)
+        self.create_scene.clicked.connect(self.CreateScene)
+        self.delete_scene.clicked.connect(self.DeleteScene)
+
+        self.open_file.clicked.connect(self.OpenFile)
+        self.save_file.clicked.connect(self.SaveFile)
+    
+    def _RaiseAMessage_(self,in_message,in_severity=hou.severityType.Message,bshow_dialog:bool=False):
+        '''
+        Handle UI Mesaage And Status Box Text
+        '''
         
+        self.state_line.setText(in_message)
+        if bshow_dialog or (in_severity==hou.severityType.Error or in_severity==hou.severityType.Warning or in_severity==hou.severityType.Fatal):
+            return hou.ui.displayMessage(in_message,severity=in_severity)
+        return 0
+    
+    def GetSelectedProject(self):
+        '''
+        Get Current Selected Project Or Display An Error
+        Returns:
+            tuple:(project_name,project_data) pr (None,None) if no selection
+        '''
+        if not self.lw_project.selectedItems:
+            self._RaiseAMessage_("Plesae Select A Project",hou.severityType.Error)
+            return None,None
+        target_name=self.lw_project.currentItem().text()
+        project_data=None
+
+        for project in self.project_data:
+            if target_name in project:
+                project_data=project[target_name]
+                break
+        print(f"Current Select Project: {target_name},Info:{project_data}")
+        return target_name,project_data
+
+
     def LoadProjectsFromConfig(self):
         '''
         List Project Data In ProjectConfig.json
         '''
         self.lw_project.clear()
-        config_path=hou.text.expandString("$MYLIB/configs/ProjectConfig.json")
         try :
-            if os.path.exists(config_path):
-                with open(config_path,"r") as file:
+            if os.path.exists(self.config_file_path):
+                with open(self.config_file_path,"r") as file:
                     self.project_data=json.load(file)
                 project_names=[]
                 for project in self.project_data:
@@ -63,123 +106,152 @@ class ProjectManager(QtWidgets.QMainWindow):
 
                 project_names.sort()
                 
-
                 self.lw_project.addItems(project_names)
 
-                self.state_line.setText(f"Finish Load {len(project_names)} projects")
+                self._RaiseAMessage_(f"Finish Load {len(project_names)} projects")
 
         except FileExistsError as error:
-            self.state_line.setText(f"Target File {config_path} NOT Existed!")
+            self._RaiseAMessage_(f"Target File {self.config_file_path} NOT Existed!")
 
     def ShowProjectDetails(self):
         print("Show detail was Called")
         if not self.lw_project.selectedItems:
-            hou.ui.displayMessage("Please Select A Project In Project List",severity=hou.severityType.Error)
+            self._RaiseAMessage_("Please Select A Project In Project List",in_severity=hou.severityType.Error)
         
-        target_project=self.lw_project.currentItem().text()
-
-        for project in self.project_data:
-            if target_project in project:
-                project_data = project[target_project]
-                break
+        target_project,project_data =self.GetSelectedProject()
         
         if project_data:
-            hou.ui.displayMessage(f"Project Detail for {'target_project'},\n"
+            self._RaiseAMessage_(f"Project Detail for {'target_project'},\n"
                                   f"Project Code {project_data['projectCode']},\n"
                                   f"Project Path {project_data['projectPath']},\n"
-                                  f"Project FPS {project_data['fps']},\n",
-                                  title=f"Detail for {target_project}")
+                                  f"Project FPS {project_data['fps']},\n",bshow_dialog=True)
         else:
-            hou.ui.displayMessage(f"Find Null Project Named {target_project}")
+            self._RaiseAMessage_(f"Find Null Project Named {target_project}",hou.severityType.Error)
     
     def ToggleProjectEnable(self,status=True):
         if not self.lw_project.selectedItems:
-            hou.ui.displayMessage("Please Select A Project In Project List",severity=hou.severityType.Error)
+            self._RaiseAMessage_("Please Select A Project In Project List",in_severity=hou.severityType.Error)
         
         env_var ={"JOB":"","CODE":"","FPS":"","PROJECT":""}
 
-        target_project=self.lw_project.currentItem().text()
-        if status==True:
-            for project in self.project_data:
-                if target_project in project:
-                    env_var.update({"JOB":project[target_project]['projectPath'],
-                                    "CODE":project[target_project]['projectCode'],
-                                    "FPS":project[target_project]['fps'],
-                                    "PROJECT":target_project})
-                    break
-            else:
-                hou.ui.displayMessage(f"Find Null Item Named {target_project}")
-                return
+        target_project,project=self.GetSelectedProject()
+        if status==True and target_project!=None:
+            env_var.update({"JOB":project['projectPath'],
+                            "CODE":project['projectCode'],
+                            "FPS":project['fps'],
+                            "PROJECT":target_project})
 
         for key,value in env_var.items():
             hou.putenv(key,value)
         
-        self.state_line.setText(f"Set Env Value To{env_var}")
-        hou.ui.displayMessage(f"Set  Env Value To{env_var}")
+        self._RaiseAMessage_(f"Set  Env Value To{env_var}",bshow_dialog=True)
 
     def DeleteProject(self):
         if not self.lw_project.selectedItems:
-            hou.ui.displayMessage("Please Select A Project In Project List",severity=hou.severityType.Error)
+            self._RaiseAMessage_("Please Select A Project In Project List",in_severity=hou.severityType.Error)
         
         target_project=self.lw_project.currentItem().text()
 
         user_choice= hou.ui.displayMessage(f"This Action Will Detele Project And All Project Files!!!",buttons=("Yes","Cancel"),severity=hou.severityType.Warning)
         if user_choice==1:
-            self.state_line.setText(f"Abandon To Delete Project {target_project}")
+            self._RaiseAMessage_(f"Abandon To Delete Project {target_project}")
             return
         
         # 删除Json
+
         for project in self.project_data:
             if target_project in project:
                 project_data = project[target_project]
                 self.project_data.remove(project)
                 break
         else:
-            hou.ui.displayMessage(f"Find Null Project Named {target_project}")
+            self._RaiseAMessage_(f"Find Null Project Named {target_project}")
             return
         
         try:
-            config_path=hou.text.expandString("$MYLIB/configs/ProjectConfig.json")
-            with open(config_path,"w") as file:
+            with open(self.config_file_path,"w") as file:
                 file.seek(0)
                 file.truncate()
                 json.dump(self.project_data,file,sort_keys=True,indent=4)
             # 刷新窗口
             self.LoadProjectsFromConfig()
-            self.state_line.setText(f"Remove Project {target_project} In Config File")
+            self._RaiseAMessage_(f"Remove Project {target_project} In Config File")
             # 删除文件
             project_dir= project_data["projectPath"]
             if os.path.exists(project_dir):
                 try:
                     shutil.rmtree(project_dir)
                 except Exception as Error:
-                    self.state_line.setText(f"Failed To Remove Project {target_project} Files: Reason{Error}")
-                    hou.ui.displayMessage(f"Failed To Remove Project {target_project} Files: Reason{Error}")
+                    self._RaiseAMessage_(f"Failed To Remove Project {target_project} Files: Reason{Error}",hou.severityType.Error)
                                 
         except Exception as Error:
-            self.state_line.setText(f"Failed To Remove Project {target_project} Reason{Error}")
+            self._RaiseAMessage_(f"Failed To Remove Project {target_project} Reason{Error}")
         
     def RefreshSceneList(self):
         self.lw_seq.clear()
         if not self.lw_project.selectedItems:
-            hou.ui.displayMessage("Please Select A Project In Project List",severity=hou.severityType.Error)
+            self._RaiseAMessage_("Please Select A Project In Project List",in_severity=hou.severityType.Error)
         
-        target_project=self.lw_project.currentItem().text()
-
-        for project in self.project_data:
-            if target_project in project:
-                project_data= project[target_project]
-                break
-        else:
-            hou.ui.displayMessage(f"Find Null Project Named {target_project}")
-            return
+        target_project,project_data=self.GetSelectedProject()
         
         seq_dir=os.path.join(project_data["projectPath"],"seq").replace(os.sep,"/")
         if not os.path.exists(seq_dir):
-            hou.ui.displayMessage(f"Find Null Sequene File In Project Named {target_project}")
+            self._RaiseAMessage_(f"Find Null Sequene File In Project Named {target_project}")
             return
         
         self.lw_seq.addItems(os.listdir(seq_dir))
+
+    def CreateScene(self):
+        #首先需要获得选中的路径
+        target_project,project_data= self.GetSelectedProject()
+        if target_project==None:
+            self._RaiseAMessage_("Please Select A Project First",hou.severityType.Error)
+            return
+        
+        project_dir:str=project_data["projectPath"]
+        project_dir.replace(os.sep,"/")
+        print(project_dir)
+        
+        if not os.path.exists(project_dir):
+            self._RaiseAMessage_(f"Give Project{target_project} 's Paths {project_dir} Doesn't Existed",hou.severityType.Error)
+            return
+        
+        lable_tint=["SceneName:","SubFolders(comma separated)"]
+        user_inputs=["NewScene","abc,tex,geo,render,cache,flip,sim"]
+        user_inputs_tuple= hou.ui.readMultiInput(message="Create New Scene",input_labels=lable_tint,buttons=("OK","Cancel"),initial_contents=user_inputs)
+        if user_inputs_tuple[0]!=0:
+            self._RaiseAMessage_("Creating New Scene was Cancelled")
+            return
+        new_scene_name=user_inputs_tuple[1][0]
+        sub_folders=user_inputs_tuple[1][1].split(",")
+        print(f"project Dir Is{project_dir}")
+        target_dir=os.path.join(project_dir,f"seq/{new_scene_name}")
+        print(f"New Seq Dir Is: {target_dir}")
+        if os.path.exists(target_dir):
+            user_override= hou.ui.displayMessage("Given Path Is Already Existed,Do You Want To Override All Files?",buttons=("OK","Cancel"))
+            if user_override==1:
+                self._RaiseAMessage_("Creating New Scene was Cancelled,Reason : Duplicated Path",hou.severityType.Error)
+                return
+        else:
+
+            os.makedirs(target_dir)
+        for sub_folder in sub_folders:
+            new_dir=os.path.join(target_dir,sub_folder).replace(os.sep,"/")
+            os.makedirs(new_dir)        
+        self._RaiseAMessage_(f"Finish Creating Seq {new_scene_name}")
+        self.RefreshSceneList()
+
+    def DeleteScene(self):
+
+        pass
+
+    def OpenFile(self):
+
+        pass
+
+    def SaveFile(self):
+
+        pass
 
 window_gui=ProjectManager()
 window_gui.show()
