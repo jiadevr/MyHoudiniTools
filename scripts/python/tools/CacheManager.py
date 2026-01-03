0fb336ef0b3b4c791a77d04f583c3a3c53b51b5f
Epslin <60341743+jiadevr@users.noreply.github.com>
2026-01-03 12:35:02 +0800

    删除历史缓存

--- scripts/python/tools/CacheManager.py
+++ scripts/python/tools/CacheManager.py
@@ -2,6 +2,7 @@ import hou
 import os
 import datetime
 import platform
+import shutil
 from PySide6 import QtCore, QtUiTools, QtWidgets, QtGui
 
 
@@ -14,9 +15,9 @@ class SceneCacheManagerUI(QtWidgets.QMainWindow):
         "rop_dop": "dopoutput",
     }
 
-    KB=1024
-    MB=KB*1024
-    GB=MB*1024
+    KB = 1024
+    MB = KB * 1024
+    GB = MB * 1024
 
     def __init__(self) -> None:
         super().__init__()
@@ -60,7 +61,7 @@ class SceneCacheManagerUI(QtWidgets.QMainWindow):
         self.cache_tree.customContextMenuRequested.connect(self._open_right_shortcut_)
 
     def _init_bindings_(self):
-        self.cleanup_button.clicked.connect(self._cleanup_)
+        self.cleanup_button.clicked.connect(lambda: self._cleanup_(True))
         self.explorer_button.clicked.connect(self._open_explore_)
         self.scan_button.clicked.connect(self.ScanScene)
 
@@ -88,7 +89,10 @@ class SceneCacheManagerUI(QtWidgets.QMainWindow):
                     if not cache_path:
                         continue
                     last_modified_str = self._get_last_modify_time_(cache_path)
-                    total_version_count,node_cache_size_sum = self._get_total_version_info_(cache_path)
+                    total_version_count, node_cache_size_sum = (
+                        self._get_total_version_info_(cache_path)
+                    )
+                    other_version_count = max(0, total_version_count - 1)
                     # relative_path = self._convert_to_relative_path(cache_path)
                     node_name, node_path, node_type_real = self._get_node_base_info_(
                         single_cache_node
@@ -99,9 +103,11 @@ class SceneCacheManagerUI(QtWidgets.QMainWindow):
                         "node_type": node_type_real,
                         "cache_path": cache_path,
                         "current_version": self._get_current_version_(node_path),
-                        "other_versions": str(total_version_count),
+                        "other_versions": str(other_version_count),
                         "lastmodified": last_modified_str,
-                        "total_size": self._convert_byte_to_bigger_unit_(node_cache_size_sum),
+                        "total_size": self._convert_byte_to_bigger_unit_(
+                            node_cache_size_sum
+                        ),
                     }
                     self._add_to_tree(node_data)
                     self.cache_data.append(node_data)
@@ -160,32 +166,60 @@ class SceneCacheManagerUI(QtWidgets.QMainWindow):
         except:
             return "N/A"
 
+    def _get_versions_path_(
+        self, in_current_version_path: str, include_current_version: bool = False
+    ) -> list:
+        """
+        获取历史版本路径数组
+        Params:
+            in_current_version_path: 当前版本存储路径
+            include_current_version: 是否包含当前版本
+        Return:
+            历史版本数组
+        """
+        versions_path = []
+        # 获得版本存储路径，本质上是利用特殊文件结构查询文件夹，文件夹以v版本号为名称
+        # 先把文件名和文件夹名拆开，再使用dirname获得v版本号的上一级目录
+        current_version_folder = os.path.split(in_current_version_path)[0]
+        cache_parent_dir = os.path.dirname(current_version_folder)
+        if not os.path.exists(cache_parent_dir):
+            return versions_path
+        for item in os.listdir(cache_parent_dir):
+            target_path = os.path.join(cache_parent_dir, item).replace(os.sep, "/")
+            if os.path.isdir(target_path) and item.startswith("v"):
+                print(
+                    f"Current Searching Path={target_path},Last version={current_version_folder},CompareReuslt:include: {include_current_version}, Is SamePath{target_path == current_version_folder},path Exist {os.path.exists(target_path)}"
+                )
+                if not os.path.exists(target_path):
+                    continue
+                # 是否排除当前路径
+                if (
+                    not include_current_version
+                    and target_path == current_version_folder
+                ):
+                    continue
+                try:
+                    # 切片器，去除v
+                    version_num_str = "".join(item[1:])
+                    if not version_num_str.isdigit():
+                        continue
+                    versions_path.append(target_path)
+                except:
+                    continue
+        return versions_path
+
     def _get_total_version_info_(self, in_cache_dir: str) -> tuple:
         """
         获取缓存路径中历史版本数量，返回文件数量和总大小,均为int
         """
-        
-        if not os.path.exists(in_cache_dir):
-            return (0,0)
-        cache_size_sum=0
+        cache_size_sum = 0
         # 先切分文件名和所在路径，再使用dirname返回上一层
-        cache_parent_dir = os.path.dirname(os.path.split(in_cache_dir)[0])
-        version = []
-        for item in os.listdir(cache_parent_dir):
-            # 它本质上是利用特殊文件结构查询文件夹，文件夹以v版本号为名称
-            target_path=os.path.join(cache_parent_dir, item)
-            if os.path.isdir(target_path) and item.startswith(
-                "v"
-            ):
-                try:
-                    # 切片器，去除v
-                    version_num = int(item[1:])
-                    cache_size_sum+=self._get_folder_size_(target_path)
-                    #print(f"Get Cache In Path{target_path},num{cache_size_sum}")
-                    version.append(version_num)
-                except:
-                    continue
-        return (len(version),cache_size_sum)
+        version = self._get_versions_path_(in_cache_dir,True)
+        if len(version) == 0:
+            return (0, 0)
+        for cache_path in version:
+            cache_size_sum += self._get_folder_size_(cache_path)
+        return (len(version), cache_size_sum)
 
     def _get_last_modify_time_(self, in_file_path: str) -> str:
         if os.path.exists(in_file_path):
@@ -263,44 +297,90 @@ class SceneCacheManagerUI(QtWidgets.QMainWindow):
         explore_action = right_shortcut.addAction("Open In Explore")
         explore_action.triggered.connect(self._open_explore_)
         clean_action = right_shortcut.addAction("Delete Old Version")
-        clean_action.triggered.connect(self._cleanup_)
+        clean_action.triggered.connect(lambda: self._cleanup_(False))
 
         right_shortcut.exec_(self.cache_tree.viewport().mapToGlobal(position))
 
-    def _cleanup_(self):
-        hou.ui.displayMessage("Call Clear Action")
+    def _cleanup_(self, clean_all=False):
         # 从缓存中删除、内容更新
+        target_nodes = self.cache_tree.selectedItems()
+        if not clean_all:
+            target_nodes = target_nodes[:1]
+        if not target_nodes:
+            hou.ui.displayMessage("Please Select A Item In List")
+            return
+        folders_to_delete = set()
+        # 获取对象缓存路径
+        for node in target_nodes:
+            # 获取历史版本计数，没有历史版本直接跳过
+            if int(node.text(5)) < 1:
+                continue
+            current_cache_path = node.text(3)
+            other_version_path = self._get_versions_path_(current_cache_path, False)
+            if len(other_version_path) < 1:
+                continue
+            # 对于包含历史版本的，直接把非当前加入统计
+            for early_version in other_version_path:
+                folders_to_delete.add(early_version)
+        if len(folders_to_delete) == 0:
+            return
+        message = "Following Folder Will Be Delete After Confirm:\n"
+        for single_folder in folders_to_delete:
+            message += f"{single_folder} \n"
+        user_input = hou.ui.displayMessage(
+            message, buttons=("DeleteAll", "NO"), severity=hou.severityType.Warning
+        )
+        # 取消删除
+        if not user_input == 0:
+            hou.ui.displayMessage("Cancel Delete", severity=hou.severityType.Message)
+            return
+        # 确认删除
+        for single_folder in folders_to_delete:
+            try:
+                shutil.rmtree(single_folder)
+            except Exception as Error:
+                hou.ui.displayMessage(
+                    f"Fail to Delete {single_folder},Reason {Error}",
+                    severity=hou.severityType.Error,
+                )
 
     def _update_stat_text_(self):
-        self.total_node.setText("Total Cache Nodes:"+str(len(self.cache_data)))
+        self.total_node.setText("Total Cache Nodes:" + str(len(self.cache_data)))
 
         total_cache_size = 0
         for node_cache_info in self.cache_data:
             target_cache_path = node_cache_info["cache_path"]
-            cache_size_of_current_node=self._get_total_version_info_(target_cache_path)[1]
-            total_cache_size+=cache_size_of_current_node
-            print(f"Node {node_cache_info['node_name']} With Cache Size:{cache_size_of_current_node},At Path{target_cache_path}")
-        self.total_size.setText("Total Cache Size:"+self._convert_byte_to_bigger_unit_(total_cache_size))
+            cache_size_of_current_node = self._get_total_version_info_(
+                target_cache_path
+            )[1]
+            total_cache_size += cache_size_of_current_node
+            print(
+                f"Node {node_cache_info['node_name']} With Cache Size:{cache_size_of_current_node},At Path{target_cache_path}"
+            )
+        self.total_size.setText(
+            "Total Cache Size:" + self._convert_byte_to_bigger_unit_(total_cache_size)
+        )
 
-    def _convert_byte_to_bigger_unit_(self,in_num_in_byte:int)->str:
-        if in_num_in_byte==0:
+    def _convert_byte_to_bigger_unit_(self, in_num_in_byte: int) -> str:
+        if in_num_in_byte == 0:
             return "--"
-        if in_num_in_byte//self.KB==0:
+        if in_num_in_byte // self.KB == 0:
             return f"{in_num_in_byte:.2f} B"
-        if in_num_in_byte//self.MB==0:
+        if in_num_in_byte // self.MB == 0:
             return f"{in_num_in_byte/self.KB:.2f} KB"
-        if in_num_in_byte//self.GB==0:
+        if in_num_in_byte // self.GB == 0:
             return f"{in_num_in_byte/self.MB:.2f} MB"
         return f"{in_num_in_byte/self.GB:.2f} GB"
-    
-    def _get_folder_size_(self,in_target_path:str)->int:
+
+    def _get_folder_size_(self, in_target_path: str) -> int:
         if not os.path.exists:
             return 0
-        folder_size=0
+        folder_size = 0
         for path, dirs, files in os.walk(in_target_path):
             for file in files:
-                folder_size+=os.path.getsize(os.path.join(path,file))
+                folder_size += os.path.getsize(os.path.join(path, file))
         return folder_size
+
 
 def ShowSceneCacheWidget():
     win = SceneCacheManagerUI()
