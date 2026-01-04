5754b27ac50d4b3545a990f2a3ed9ac7c353defe
Epslin <60341743+jiadevr@users.noreply.github.com>
2026-01-04 17:43:33 +0800

    全选、弃选逻辑及CreateMaterial框架

--- scripts/python/tools/tex_to_mtlx.py
+++ scripts/python/tools/tex_to_mtlx.py
@@ -92,7 +92,7 @@ class TxToMtlx(QtWidgets.QMainWindow):
         self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
 
         ## DATA
-        self.mtlTX = False
+        self.mtlTX: bool = False
 
         self._setup_help_section()
         self._setup_material_section()
@@ -104,8 +104,9 @@ class TxToMtlx(QtWidgets.QMainWindow):
         self.material_lib_node: hou.OpNode = hou.node(
             "/shop"
         )  # <---------Remove After Test
-        self.tex_collection:dict={}
-
+        # 用户选择的纹理文件夹路径
+        self.tex_folder: str = ""
+        self.tex_collection: dict = {}
 
     def _setup_help_section(self):
         """Setup the help button section"""
@@ -189,6 +190,10 @@ class TxToMtlx(QtWidgets.QMainWindow):
         self.bt_instructions.clicked.connect(self._show_help_)
         self.bt_lib.clicked.connect(self._set_material_lib_)
         self.bt_open_folder.clicked.connect(self._open_file_browser_)
+        self.bt_sel_all.clicked.connect(self._select_all_in_matlist_)
+        self.bt_sel_non.clicked.connect(self._deselect_all_in_matlist_)
+        self.bt_create.clicked.connect(self._create_materials_)
+        self.checkbox.stateChanged.connect(self._change_use_mtlTX_)
 
     def _show_help_(self):
         message = """
@@ -241,6 +246,7 @@ class TxToMtlx(QtWidgets.QMainWindow):
         """
         打开包含纹理的文件路径
         """
+        self.model.clear()
         folder_path: str = QtWidgets.QFileDialog.getExistingDirectory(
             self, "Please Select Your Texture Folder"
         )
@@ -254,14 +260,17 @@ class TxToMtlx(QtWidgets.QMainWindow):
             )
             return
         # 查找图片信息
-        self.tex_collection=self._collect_images_in_dir(folder_path)
-        if len(self.tex_collection)==0:
+        self.tex_folder = folder_path
+        self.tex_collection = self._collect_images_in_dir(folder_path)
+        if len(self.tex_collection) == 0:
             return
-        #pprint.pprint(self.tex_collection)
+        # pprint.pprint(self.tex_collection)
         # 把图片信息添加到List
-        self.model.clear()
         for tex_group in self.tex_collection:
             self.model.appendRow(QtGui.QStandardItem(tex_group))
+        self.bt_sel_all.setEnabled(True)
+        self.bt_sel_non.setEnabled(True)
+        self.bt_create.setEnabled(True)
 
     def _contain_any_image_file_(self, in_dir_path: str) -> bool:
         """
@@ -283,45 +292,123 @@ class TxToMtlx(QtWidgets.QMainWindow):
                     return True
         return False
 
-    def _collect_images_in_dir(self, in_dir_path: str)->dict:
+    def _collect_images_in_dir(self, in_dir_path: str) -> dict:
         # 解析文件夹中的纹理，构造一个二维字典
         if not os.path.exists(in_dir_path):
             hou.ui.displayMessage("Given Path Is Not Valid,Please Check")
             return {}
-        material_collection=defaultdict(lambda:defaultdict(list))
+        material_collection = defaultdict(lambda: defaultdict(list))
         for elem in os.listdir(in_dir_path):
             current_testing_path = os.path.join(in_dir_path, elem)
             if not os.path.isfile(current_testing_path):
                 continue
             if not elem.endswith(self.SUPPORT_IMAGE_FORMAT):
                 continue
-            file_name=elem.split(".")[0].lower()
+            file_name = elem.split(".")[0].lower()
             # 使用_分段
-            name_keywords=file_name.split("_")
-            material_name=name_keywords[0]
+            name_keywords = file_name.split("_")
+            material_name = name_keywords[0]
             # 使用关键词匹配纹理种类
-            texure_type=""
+            texure_type = ""
             for type in self.TEX_TYPE:
                 for name_keyword in name_keywords[1:]:
                     if type in name_keyword:
-                        texure_type=name_keyword
+                        texure_type = name_keyword
                         break
             # 没有匹配的字段，纹理不加入纹理组
-            if texure_type=="":
+            if texure_type == "":
                 continue
             material_collection[material_name][texure_type].append(elem)
             # 使用正则表达式获取纹理分辨率
-            tex_res=self.ORDINARY_TEX_REGEX_PATTERN.search(elem)
-            if not tex_res==None:
+            tex_res = self.ORDINARY_TEX_REGEX_PATTERN.search(elem)
+            if not tex_res == None:
                 material_collection[material_name]["res"].append(tex_res.group(1))
             # 使用正则表达式匹配UDIM
-            is_UDIM= self.UDIM_TEX_REGEX_PATTERN.search(elem)
-            if not is_UDIM==None:
+            is_UDIM = self.UDIM_TEX_REGEX_PATTERN.search(elem)
+            if not is_UDIM == None:
                 material_collection[material_name]["UDIM"].append(is_UDIM)
-            
-        if len(material_collection)==0:
+
+        if len(material_collection) == 0:
             return {}
-        return dict(material_collection)
+        result: dict = {}
+        for name, tex_data in material_collection.items():
+            result[name] = dict(tex_data)
+        return result
+
+    def _change_use_mtlTX_(self, status):
+        if status == QtCore.Qt.CheckState.Checked:
+            self.mtlTX = True
+        else:
+            self.mtlTX = False
+        print(f"Use mtlTX:{self.mtlTX}")
+
+    def _select_all_in_matlist_(self):
+        # self.material_list.selectAll()
+        # 另一种选择方式
+        selection_model=self.material_list.selectionModel()
+        for row in range(self.model.rowCount()):
+            index=self.model.index(row,0)
+            selection_model.select(index,QtCore.QItemSelectionModel.SelectionFlag.Select)
+
+    def _deselect_all_in_matlist_(self):
+        self.material_list.clearSelection()
+
+    def _create_materials_(self):
+        # 重置状态
+        self.progress_bar.setValue(0)
+        selected_mat_items = self.material_list.selectedIndexes()
+        if len(selected_mat_items) == 0:
+            hou.ui.displayMessage(
+                "Please Select A Least One Material",  severity=hou.severityType.Error
+            )
+            return
+        self.progress_bar.setMaximum(len(selected_mat_items))
+        current_progress = 0
+        base_info = {
+            "b_use_mtlTX": self.mtlTX,
+            "node_path": self.material_lib_path,
+            "node_ref": self.material_lib_node,
+            "tex_folder_path": self.tex_folder,
+        }
+        for item in selected_mat_items:
+            item_index = item.row()
+            # 这有点问题，dict的key是保序的么
+            material_name = list(self.tex_collection.keys())[item_index]
+            material_creator = MtlxMaterial(
+                material_name, **base_info, texture_list=self.tex_collection
+            )
+            material_creator.create_material()
+
+            current_progress += 1
+            self.progress_bar.setValue(current_progress)
+
+        hou.ui.displayMessage("Finish Creating Material", severity=hou.severityType.Message)
+
+
+class MtlxMaterial:
+    def __init__(
+        self,
+        mat_name,
+        b_use_mtlTX,
+        node_path,
+        node_ref,
+        tex_folder_path,
+        texture_list,
+    ) -> None:
+        self.mat_name = mat_name
+        self.b_mtlTX = b_use_mtlTX
+        self.node_path = node_path
+        self.node_ref = node_ref
+        self.tex_folder_path = tex_folder_path
+        self.texture_list = texture_list
+
+    def create_material(self):
+        print(self.mat_name)
+        print(self.b_mtlTX)
+        print(self.node_path)
+        print(self.node_ref)
+        print(self.tex_folder_path)
+        print(self.texture_list)
 
 
 def ShowTexToMatTool():
